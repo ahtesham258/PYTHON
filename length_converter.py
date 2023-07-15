9d202aaf70a2c8a187ff4366ebde339807f4e376
MD AHTESHAMUL HAQUE <110822495+ahtesham258@users.noreply.github.com>
2023-07-16 05:28:54 +0530

    Length Converter

---
+++ length_converter.py
@@ -0,0 +1,21 @@
+#
+
+# kilometres= input(float ("Enter the Kilometre"))
+miles= input (float("Enter the miles"))
+
+miles_to_kilometres=miles*1.61
+kilometres_to_miles=kilometres/1.61
+
+
+print( miles, "miles is", round(miles_to_kilometres,2), "kilometres")
+print(kilometres, "kilometres is", round(kilometres_to_miles,2) ,"miles")
+# 
+
+miles = 7.38
+kilometers = 12.25
+
+miles_to_kilometers = miles * 1.60934
+kilometers_to_miles = kilometers / 1.60934
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
